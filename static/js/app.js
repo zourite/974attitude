c43d10400de396cc6b4f0eeab9d4f4a9bfb7a84b
Sonia SAUGRIN <ssaugrin@gmail.com>
2020-04-14 00:13:14 -0300

    improve mobile ui

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,6 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.sidenav');
-//     var instances = M.Sidenav.init(elems, options);
-//   });
-
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
   $(window).on( 'load', function(){
 
     $('#masonry').masonry({
